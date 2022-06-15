befc36c164c526be5c1a124a4e701299c0a8119d
Name Surname <gargpriya5419@gmail.com>
2022-06-15 23:23:32 +0530

    Added num-to-string solution

---
+++ 8kyu/Convert-a-Number-to-a-String.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+// SOLUTION 
+
+function numberToString(num) {
+
+    // toString() method takes an integer or floating point number and converts it into a String type.
+
+    return num.toString();
+
+}
+
+console.log(numberToString(3));
